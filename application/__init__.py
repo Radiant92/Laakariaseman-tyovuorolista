baf9d4a4e76a34f317b846fc73d614ffc0e22fc1
Martin Strohm <martin.strohm@helsinki.fi>
2019-01-31 21:42:49 +0200

    typo initissä

--- application/__init__.py
+++ application/__init__.py
@@ -8,13 +8,9 @@ import os
 if os.environ.get("HEROKU"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"    
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"    
     app.config["SQLALCHEMY_ECHO"] = True
 
-  
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
-app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
