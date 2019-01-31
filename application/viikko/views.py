c062fc4241bebef13cb3b38821c288510b435ee4
Martin Strohm <martin.strohm@helsinki.fi>
2019-01-31 22:02:53 +0200

    selvittelen miksi heroku ei toimi, latasin uudelleen requirements.txt ja samalla toiminnallisuuden sille ettei muut näe toistensa viikkoja

--- application/viikko/views.py
+++ application/viikko/views.py
@@ -9,7 +9,7 @@ from application.viikko.forms import ViikkoForm
 @app.route("/viikot", methods=["GET"])
 @login_required
 def viikot_index():
-    return render_template("viikot/list.html", viikot = Viikko.query.order_by("vuosi desc").all())
+    return render_template("viikot/list.html", viikot = Viikko.query.filter(Viikko.account_id == current_user.id).order_by("vuosi desc").all())
 
 @app.route("/viikot/new/")
 @login_required
@@ -26,7 +26,7 @@ def viikot_create():
 
     viikko = Viikko(form.vuosi.data,form.numero.data)
     viikko.account_id = current_user.id
-    
+
     db.session().add(viikko)
     db.session().commit()
     return redirect(url_for("viikot_index"))
