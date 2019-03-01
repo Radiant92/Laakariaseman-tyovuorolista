445840442ac3532f759998bc90eb1014f8f1785b
Martin Strohm <martin.strohm@helsinki.fi>
2019-03-01 20:32:48 +0200

    paiva modelin kyselyn muokkaus postgre ystävälliseksi

--- application/paiva/models.py
+++ application/paiva/models.py
@@ -2,6 +2,7 @@ from application import db
 from application.models import BaseTila
 from application.tunti.models import Tunti
 from sqlalchemy.sql import text
+from application.auth.models import User
 class Paiva(BaseTila):
     __tablename__ = "paiva"
 
@@ -27,14 +28,15 @@ class Paiva(BaseTila):
                 return tunti
 
     def get_is_empty_for_user(self, user):
-        stmt = text("SELECT account.username, paiva.id From paiva, tunti, tunti_user, account "
+        stmt = text("SELECT account.id, paiva.id From paiva, tunti, tunti_user, account "
                     "WHERE paiva.id = tunti.paiva_id "
                     "AND tunti_user.tunti_id = tunti.id "
                     "AND account.id = tunti_user.account_id "
-                    "GROUP BY tunti.id")
+                    "GROUP BY tunti.id, paiva.id")
         res = db.engine.execute(stmt)
         for row in res:
-            if row[0] == user.username and self.id == int(row[1]):
+            useri = User.query.filter(User.id == int(row[0])).first()
+            if useri.username == user.username and self.id == int(row[1]):
                 return True
         return False
 
